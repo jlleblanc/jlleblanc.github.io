2669b36cec0ce466f2c8a990aeed436605814c84
Joe LeBlanc <joe@truelinkfinancial.com>
2020-05-25 13:55:53 -0500

    Added about.ts deno module

---
+++ about.ts
@@ -0,0 +1,9 @@
+console.log("Joseph L. LeBlanc");
+console.log("");
+console.log("Hello! I am a Software Engineering Manager. I have been coding as a hobby since I was 8 years old and professionally since 2004.");
+console.log("");
+console.log("GitHub: https://github.com/jlleblanc");
+console.log("");
+console.log("LinkedIn: https://www.linkedin.com/in/jlleblanc");
+console.log("");
+console.log("Email: contact@jlleblanc.com");
