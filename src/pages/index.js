817a36ef0db5487257bbcd6cbb6c601a7df16361
Joe LeBlanc <contact@jlleblanc.com>
2024-09-10 18:19:04 -0400

    Scaling down the words.

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,24 +27,23 @@ export default function Home() {
       jsonLd={jsonLd}
     >
       <h1 className="text-4xl font-bold mb-4">{siteConfig.title}</h1>
-      <h2 className="text-3xl mb-8">Delivering today and planning for tomorrow.</h2>
+      <h2 className="text-3xl mb-8">Crucial direction at the right time.</h2>
       <div className="flex flex-col sm:flex-row items-center sm:items-center mb-8">
         <img src="https://github.com/jlleblanc.png" alt="Joe" className="w-36 h-36 rounded-full mb-4 sm:mb-0 sm:mr-8 border border-gray-200 shadow-sm" />
-        <div className="text-xl sm:text-left sm:self-center">
+        <div className="text-2xl sm:text-left sm:self-center">
           <p>
-            Hi, I'm Joe! I'm a software engineering leader helping startups build great products and services.
-            I bring years of experience to help you move fast now, while planning ahead for the future.
+            Hi, I'm Joe! I'm a hands-on software engineering leader of products, teams, and startups.
           </p>
           <p className="mt-3">
-            Whether you're an entrepreneur seeking technical direction, an experienced developer moving into leadership, or an early career software engineer, I'm here to help.
+
           </p>
         </div>
       </div>
-      <h3 className="text-2xl font-semibold mb-6">Elevate Your Tech Journey. I can help.</h3>
+      <h3 className="text-2xl font-semibold mb-6">Move fast today. Scale for tomorrow.</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-9 mb-8">
         <div className="bg-white p-6 rounded-lg shadow-md">
-          <h4 className="text-xl font-semibold mb-2">Strategic Tech Planning</h4>
-          <p>Choose technology serving your business goals using cutting-edge tools like Next.js and Supabase.</p>
+          <h4 className="text-xl font-semibold mb-2">Strategic Technical Planning</h4>
+          <p>Choose technology serving short-term needs and long-term goals.</p>
         </div>
         <div className="bg-white p-6 rounded-lg shadow-md">
           <h4 className="text-xl font-semibold mb-2">AI-Driven Innovation</h4>
