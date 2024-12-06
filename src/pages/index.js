9357e4195f5c98529bcd455ec17245e442510a47
Joe LeBlanc <contact@jlleblanc.com>
2024-12-06 14:19:34 -0500

    Ship it!

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,8 +27,8 @@ export default function Home() {
       jsonLd={jsonLd}
     >
       <div className="container mx-auto px-4 max-w-3xl">
-      <h2 className="text-3xl font-bold mb-16 mt-8">{siteConfig.title}</h2>
-      <div className="flex flex-col sm:flex-row items-center sm:items-center mb-8 mt-8">
+      <h2 className="text-3xl font-bold mt-8">{siteConfig.title}</h2>
+      <div className="flex flex-col sm:flex-row items-center sm:items-center mt-8">
           <img src="https://github.com/jlleblanc.png" alt="Joe" className="w-36 h-36 rounded-full mb-4 sm:mb-0 sm:mr-8 border border-gray-200 shadow-sm" />
           <div className="text-xl sm:text-left sm:self-center">
             <p>
@@ -39,22 +39,8 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col gap-8 mt-12">
-          {/* <div>
-            <h3 className="text-xl font-semibold mb-2">Architectural Expertise</h3>
-            <p>I keep track of the cookies, the databases, and servers, while you keep your paying customers happy.</p>
-          </div>
-          <div>
-            <h3 className="text-xl font-semibold mb-2">Author</h3>
-            <p>Over a dozen books and video series authored, most recently <Link className="text-blue-500" href="https://www.linkedin.com/learning/supabase-essential-training/developing-application-backends-using-supabase">Supabase Essential Training</Link> on LinkedIn Learning.</p>
-          </div>
-          <div>
-            <h3 className="text-xl font-semibold mb-2">Leadership Development</h3>
-            <p>Nurture your team&apos;s potential and create a culture of continuous improvement.</p>
-          </div> */}
-        </div>
       </div>
-      <div className="text-center">
+      <div className="text-center mt-12">
         <Link href="/contact" className="border-2 border-emerald-800 text-emerald-800 text-grey-900 font-bold py-3 px-6 rounded-full text-xl hover:border-teal-500 transition duration-300">
           Get In Touch
         </Link>
